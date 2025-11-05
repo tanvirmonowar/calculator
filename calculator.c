df1b23fa9e80ab90495203ade8716568c3741d2f
tanvirmonowar <tanvirmonowar5@gmail.com>
2025-11-06 05:43:01 +0600

    Adding callculator

---
+++ calculator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{	double a,b;
+	char c;
+	printf("Enter two number:");
+	scanf("%lf %lf",&a,&b);
+	printf("Enter an operator:");
+	scanf(" %c",&c);
+	switch(c){
+		case'+':
+			printf("Result:%0.2lf",a+b);
+			break;
+		case'-':
+			printf("Result:%0.2lf",a-b);
+			break;
+		case'*':
+			printf("Result:%0.2lf",a*b);
+			break;
+		case'/':
+			if (b=!0)
+			printf("Result:%0.2lf",a/b);
+			else
+			printf("Result:0");
+			break;
+			
+		default:
+	printf("Invlid operator");
+		}	
+		return 0;
+		}
